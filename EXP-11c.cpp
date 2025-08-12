da0672e245ee30f5153df3093684c4c231cd336a
Krish2619 <krishna.chhabaria.btech2024@sitpune.edu.in>
2025-08-12 15:10:51 +0530

    Add files via upload

---
+++ EXP-11c.cpp
@@ -0,0 +1,27 @@
+//Krishna Chhabaria
+//24070123149
+//B3
+#include<iostream>
+using namespace std;
+class cube {
+    private:
+    int height = 2.0;
+    int width = 3.0;
+    int length = 5.0;
+    public:
+    int volume() {
+        int v;
+        v = height * width * length;
+        return v;
+    }
+};
+int main() {
+    cube cube1;
+    int vol = cube1.volume();
+    cout << "Volume of the cube is: " << vol << endl;
+    return 0;
+}
+
+/*output:
+Volume of the cube is: 30
+*/
